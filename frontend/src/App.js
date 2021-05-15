abc87193610183b891d6cd2070d516320ca51066
Frank Steidinger <steidinger@acm.org>
2021-05-15 11:16:14 +0200

    add county cards with real data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
+import {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, Card, CardActions, CardMedia, Fab, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Fab, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import InzidenzCard from './components/InzidenzCard';
@@ -19,9 +20,17 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-
 function App() {
   const classes = useStyles();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const url = 'https://serverless-corona-inzidenz-269004290177-dev.s3.eu-central-1.amazonaws.com/data.json';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => setError(error));
+  }, []);
   return (
     <>
       <AppBar position="static">
@@ -32,7 +41,10 @@ function App() {
           <Typography variant="h6" className={classes.title}>Corona Inzidenz</Typography>
         </Toolbar>
       </AppBar>
-      <InzidenzCard county="LK Esslingen" inzidenzToday={132} />
+      {error ?? <Typography variant="body1">{error}</Typography>}
+      <InzidenzCard county="LK Esslingen" data={data} />
+      <InzidenzCard county="SK Stuttgart" data={data} />
+      <InzidenzCard county="LK Schwarzwald-Baar-Kreis" data={data} />
       <Fab color="primary" aria-label="add" className={classes.fab}>
         <AddIcon />
       </Fab>
--- frontend/src/components/InzidenzCard.js
+++ frontend/src/components/InzidenzCard.js
@@ -1,20 +1,62 @@
+import {useMemo} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, Card, CardActions, CardMedia, Fab, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { Card, Grid, Typography } from '@material-ui/core';
+import {Skeleton} from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
     card: {
       margin: theme.spacing(2),
       padding: theme.spacing(1),
     },
+    mainValue: {
+        textAlign: 'center',
+    }
   }));
   
+function findCounty(data, county) {
+    if (!data) {
+        return null;
+    }
+    const countyData = data.find(({name}) => name === county);
+    if (!countyData || !Array.isArray(countyData.inzidenz)) {
+        return {
+            inzidenzToday: {
+                value: 'n/a',
+                date: '--',
+            }
+        }
+    }
+    const inzidenzToday = countyData.inzidenz.slice(-1)[0];
+    const inzidenzYesterday = countyData.inzidenz.slice(-2)[0];
+    const change = inzidenzToday.value - inzidenzYesterday.value;
+    const changePercent = Math.round(change * 100 / inzidenzYesterday.value);
+    return {
+        inzidenzToday: {
+            value:new Intl.NumberFormat('de-DE', {maximumFractionDigits: 0}).format(inzidenzToday.value),
+            date: inzidenzToday.date,
+        }, 
+        change: new Intl.NumberFormat('de-DE', {signDisplay: 'always', maximumFractionDigits: 0}).format(change), 
+        changePercent: new Intl.NumberFormat('de-DE', {signDisplay: 'always', maximumFractionDigits: 1}).format(changePercent)
+    };
+}
 
-export default function InzidenzCard({county, inzidenzToday}) {
+export default function InzidenzCard({county, data}) {
     const classes = useStyles();
+    const countyData = useMemo(() => findCounty(data, county), [data, county]);
+    const {inzidenzToday, change, changePercent} = countyData ?? {};
     return (
       <Card className={classes.card}>
         <Typography variant="caption">{county}</Typography>
-        <Typography variant="h1">{inzidenzToday}</Typography>
+        <Typography variant="h1" className={classes.mainValue}>{inzidenzToday?.value ?? <Skeleton />}</Typography>
+        <Grid container justify="space-between">
+            <Grid item>
+                <Typography variant="body1">{change}</Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant="body1">{changePercent}%</Typography>
+            </Grid>
+        </Grid>
+        <Typography variant="caption">Stand: {inzidenzToday?.date ?? <Skeleton />}</Typography>
       </Card>
     )
   }
