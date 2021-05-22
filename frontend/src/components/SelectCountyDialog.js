c348d55503e8ce18f178aea6f1b7346ff7221c5a
Frank Steidinger <steidinger@acm.org>
2021-05-22 10:16:05 +0200

    filter counties only if search text longer than 3 chars
    Delay rendering of counties list when dialog is opened.
    Dialog opens more quickly this way.

--- frontend/src/components/SelectCountyDialog.js
+++ frontend/src/components/SelectCountyDialog.js
@@ -1,10 +1,17 @@
-import { useCallback, useState } from 'react';
-import { Button, Checkbox, Dialog, DialogContent, DialogTitle, DialogActions, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { useCallback, useEffect, useMemo, useReducer } from 'react';
+import { Button, Checkbox, Dialog, DialogContent, DialogTitle, DialogActions, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
 import {Clear, Search} from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Counties({counties, selectedCounties, onToggle}) {
-    return (
-        <List dense button>
+const useStyles = makeStyles(() => ({
+    counties: {
+        minWidth: '25em',
+    }
+}));
+
+function Counties({className, counties, selectedCounties, onToggle}) {
+    return useMemo(() =>
+        <List dense className={className}>
             {counties.map(name =>
                 <ListItem button onClick={() => onToggle(name)} key={name}>
                     <ListItemIcon>
@@ -13,59 +20,105 @@ function Counties({counties, selectedCounties, onToggle}) {
                     <ListItemText>{name}</ListItemText>
                 </ListItem>)
             }
-        </List>);
+        </List>, [counties, selectedCounties, onToggle]);
+}
+
+const initialState = {
+    initialized: false,
+    searchText: '',
+    selectedCounties: new Set(),
+    allCounties: [],
+    filteredCounties: [],
+}
+
+const MIN_FILTER_LENGTH = 3;
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'init':
+            return {
+                ...state,
+                initialized: true,
+                allCounties: action.counties,
+                filteredCounties: action.counties,
+                selectedCounties: action.selectedCounties,
+            }
+        case 'change_search': {
+            let newFilteredCounties;
+            if (!action.value || (action.value.length < MIN_FILTER_LENGTH && state.searchText.length >= MIN_FILTER_LENGTH)) {
+                newFilteredCounties = state.allCounties;
+            }
+            else if (action.value.length >= MIN_FILTER_LENGTH) {
+                const term = action.value.toLowerCase();
+                newFilteredCounties = state.allCounties.filter(county => county.toLowerCase().indexOf(term) !== -1);
+            }
+            else {
+                newFilteredCounties = state.filteredCounties;
+            }
+            return {
+                ...state,
+                searchText: action.value,
+                filteredCounties: newFilteredCounties,
+            }
+        }
+        case 'toggle_county': {
+            const selectedCounties = new Set(state.selectedCounties);
+            if (selectedCounties.has(action.county)) {
+                selectedCounties.delete(action.county);
+            }
+            else {
+                selectedCounties.add(action.county);
+            }
+            return {
+                ...state,
+                selectedCounties,
+            }
+        }
+        default:
+            return state;
+    }
 }
 
 export default function SelectCountyDialog({ allCounties, activeCounties, onApply, onCancel }) {
-    const [selectedCounties, setSelectedCounties] = useState(new Set(activeCounties));
-    const [searchTerm, setSearchTerm] = useState('');
-    const [counties, setCounties] = useState(allCounties);
-    
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const {initialized, filteredCounties, searchText, selectedCounties} = state;
+    const classes = useStyles();
+
+    useEffect(() => setTimeout(() => 
+        dispatch({type: 'init', counties: allCounties, selectedCounties: new Set(activeCounties)})
+        , 1), [allCounties, activeCounties]);
+
     const updateSearch = useCallback(function (event) {
-        let term = event.target.value;
-        setSearchTerm(event.target.value);
-        if (term) {
-            term = term.toLowerCase();
-            setCounties(allCounties.filter(county => county.toLowerCase().indexOf(term) !== -1));
-        }
-        else {
-            setCounties(allCounties);
-        }
-    }, [allCounties]);
+        dispatch({type: 'change_search', value: event.target.value});
+    }, []);
 
     const clearSearch = useCallback(function () {
-        setSearchTerm('');
-        setCounties(allCounties);
-    }, [allCounties]);
+        dispatch({type: 'change_search', value: ''});
+    }, []);
 
-    function toggle(name) {
-        const newState = new Set(selectedCounties);
-        if (selectedCounties.has(name)) {
-            newState.delete(name);
-        }
-        else {
-            newState.add(name);
-        }
-        setSelectedCounties(newState);
-    }
+    const toggle = useCallback(function (name) {
+        dispatch({type: 'toggle_county', county: name})
+    }, []);
 
     return (
         <Dialog open={true} scroll="paper" className="county-dialog">
             <DialogTitle>
                 <div>Angezeigte Kreise auswählen</div>
                 <Input 
-                    type="search" 
-                    value={searchTerm} 
+                    value={searchText} 
                     onChange={updateSearch} 
                     endAdornment={
                         <InputAdornment position="end">
-                            {searchTerm ? <IconButton onClick={clearSearch}><Clear /></IconButton> : <Search />}
+                            {searchText ? <IconButton onClick={clearSearch}><Clear /></IconButton> : <Search />}
                         </InputAdornment>
                     }
                 />
             </DialogTitle>
-            <DialogContent>
-                <Counties counties={counties} selectedCounties={selectedCounties} onToggle={toggle} />
+            <DialogContent className={classes.counties}>
+                {initialized 
+                    ? <Counties counties={filteredCounties} selectedCounties={selectedCounties} onToggle={toggle} />
+                    : <Typography variant="body1">Lade...</Typography>
+                }
             </DialogContent>
             <DialogActions>
                 <Button onClick={onCancel}>Abbrechen</Button>
